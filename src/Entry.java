c5a6533d4b391ca15e5c4cc6c0b274fb46c6bc9b
markd315 <markd315@gmail.com>
2018-02-03 22:40:46 -0500

    Minor syntax misses

--- src/Entry.java
+++ src/Entry.java
@@ -25,21 +25,21 @@ public class Entry {
 	}
 
 	public int sumOfIns() {//TODO I WROTE THIS DRUNK AND IT NEEDS A UNIT TEST
-		int ret;
+		int ret =0;
 		for(int inputIndexIndex=0; inputIndexIndex<inputIndices.size(); inputIndexIndex++){
 		for(int i=0; i<parentLedger.getBlockchain().size(); i++) {//This runs in constant time with the number of inputs.
 			if(this.getInputIDs().contains(parentLedger.getBlockchain().get(i).getId())) {//
 				//We found an input to THIS transaction entry.
 				//So now what we do is sum up the INDEX OUTPUT of those valid transactions that input here.
 				//We need to use the input index to know WHICH one is the input.
-				ret+= parentLedger.getBlockchain().get(i).getOutputAmounts().get(inputIndices.get(j));//We need to find the 
+				ret+= parentLedger.getBlockchain().get(i).getOutputAmounts().get(inputIndices.get(inputIndexIndex));//We need to find the 
 			}
 			}
 		}
 		return ret;
 	}
 
-	private List<String> getOutputAmounts() {
+	private List<Integer> getOutputAmounts() {
 		return outputAmounts;
 	}
 
@@ -53,7 +53,7 @@ public class Entry {
 
 	public int sumOfOuts() {
 		// TODO Auto-generated method stub
-		return null;
+		return 0;
 	}
 
 }
