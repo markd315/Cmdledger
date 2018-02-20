f0995b2b132175bc7fa3e3fac2332d8c96f91415
markd315 <markd315@gmail.com>
2018-02-20 12:30:29 -0500

    Fixed some quote encoding issues from copy pasting help text.

--- src/CmdLedger.java
+++ src/CmdLedger.java
@@ -83,17 +83,17 @@ public class CmdLedger {
 			}
 			if (cmd.equalsIgnoreCase("h")) {
 				System.out.println(
-						"[F]ile:  Supply filename:<infilename>.  Read in a file of transactions. Any invalid transaction shall be identified with an error message to stderr, but not stored. Print an error message to stderr if the input file named cannot be opened. The message shall be “Error: file <infilename> cannot be opened for reading” on a single line, where <infilename> is the name provided as additional command input.  \r\n"
+						"[F]ile:  Supply filename:<infilename>.  Read in a file of transactions. Any invalid transaction shall be identified with an error message to stderr, but not stored. Print an error message to stderr if the input file named cannot be opened. The message shall be Error: file <infilename> cannot be opened for reading on a single line, where <infilename> is the name provided as additional command input.  \r\n"
 								+ "\r\n"
 								+ "[T]ransaction: Supply Transaction:<see format below>   Read in a single transaction in the format shown below.  It shall be checked for validity against the ledger, and added if it is valid. If it is not valid, then do not add it to the ledger and print a message to stderr with the transaction number followed by a colon, a space, and the reason it is invalid on a single line.\r\n"
 								+ "\r\n" + "[E]xit:  Quit the program\r\n" + "\r\n"
 								+ "[P]rint:  Print current ledger (all transactions in the order they were added) to stdout in the transaction format given below, one transaction per line.\r\n"
 								+ "\r\n" + "[H]elp:  Command Summary\r\n" + "\r\n"
-								+ "[D]ump:  Supply filename:<outfilename>.  Dump ledger to the named file. Print an error message to stderr if the output file named cannot be opened. The message shall be “Error: file <outfilename> cannot be opened for writing” on a single line, where <outfilename> is the name provided as additional command input. \r\n"
+								+ "[D]ump:  Supply filename:<outfilename>.  Dump ledger to the named file. Print an error message to stderr if the output file named cannot be opened. The message shall be Error: file <outfilename> cannot be opened for writing on a single line, where <outfilename> is the name provided as additional command input. \r\n"
 								+ "\r\n" + "[W]ipe:  Wipe the entire ledger to start fresh.\r\n" + "\r\n"
 								+ "[I]nteractive: Toggle interactive mode. Start in non-interactive mode, where no command prompts are printed. Print command prompts and prompts for additional input in interactive mode, starting immediately (i.e., print a command prompt following the I command).\r\n"
 								+ "\r\n"
-								+ "[V]erbose: Toggle verbose mode. Start in non-verbose mode. In verbose mode, print additional diagnostic information as you wish. At all times, output each transaction number as it is read in, followed by a colon, a space, and the result (“good” or “bad”). \r\n"
+								+ "[V]erbose: Toggle verbose mode. Start in non-verbose mode. In verbose mode, print additional diagnostic information as you wish. At all times, output each transaction number as it is read in, followed by a colon, a space, and the result (good or bad). \r\n"
 								+ "\r\n"
 								+ "[B]alance:  Supply username:  (e.g. Alice).  This command prints the current balance of a user.");
 			}
